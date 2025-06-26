ecf75601b548ac7c545d01b75bdc95511e308daf
Jefferson Rosa <jefmi.98@outlook.com>
2025-06-26 18:05:41 -0300

    Criando projeto ASP.Net Criando menu animado Criando .gitignore

---
+++ Portfolio/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿const iconMenu = document.getElementById('iconMenu');
+const menuPort = document.querySelector('.Menu-Port');
+
+iconMenu.addEventListener('click', () => {
+    const isAberto = menuPort.classList.toggle('mostrar');
+
+    // Alterna os ícones da porta
+    iconMenu.className = isAberto
+        ? 'fa-solid fa-door-open icone-menu botao-icone'
+        : 'fa-solid fa-door-closed icone-menu botao-icone';
+});
